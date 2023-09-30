099efe5c98117fc71a39eca998066433fdbadf9f
Tsaone <tsaoneb3@gmail.com>
2023-09-30 08:08:36 +0200

    added an event listener to JS

--- script.js
+++ script.js
@@ -2,6 +2,21 @@ const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random'
 const quoteDisplayElement = document.getElementById('quoteDisplay')
 const quoteInputElement = document.getElementById('quoteInput')
 
+quoteDisplayElement.addEventListener('input', () => {
+    const arrayQuote = quoteDisplayElement.querySelectorAll('span')
+    const arrayValue = quoteInputElement.value.split('')
+    arrayQuote.forEach((characterSpan, index) => {
+        const character = arrayValue[index]
+        if (character === characterSpan.innerText) {
+            characterSpan.classList.add('correct')
+            characterSpan.classList.remove('incorrect')
+        } else {
+            characterSpan.classList.remove('correct')
+            characterSpan.classList.remove('incorrect')
+        }
+    })
+})
+
 function getRandomQuote() {
     return fetch (RANDOM_QUOTE_API_URL)
     .then(response => response.json())
