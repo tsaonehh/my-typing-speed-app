cbe54dfa3b25b32fef8424dd18739c98445e174a
Tsaone <tsaoneb3@gmail.com>
2023-10-01 22:01:29 +0200

    started working on timer and removed default values

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random'
 const quoteDisplayElement = document.getElementById('quoteDisplay')
 const quoteInputElement = document.getElementById('quoteInput')
+const timerElement = document.getElementById('timer')
 
 quoteInputElement.addEventListener('input', () => {
     const arrayQuote = quoteDisplayElement.querySelectorAll('span')
@@ -41,6 +42,10 @@ async function renderNewQuote(){
         quoteDisplayElement.appendChild(characterSpan)
     })
     quoteInputElement.value = null
+}
+
+function startTimer() {
+    timerElement.innerText = 0
 }
 
 renderNewQuote()
